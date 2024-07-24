cd8ebd1ea9bfcd38a8e739ced5c0d96c41872308
RishabhShrival <rishabhshrival746@gmail.com>
2024-07-24 20:16:13 +0530

    firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,13 +10,13 @@ import {getDatabase} from "firebase/database"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkFlbuk1yUK79JjbjuEssiU71HOEuWPWw",
-  authDomain: "testing-fb3fa.firebaseapp.com",
-  projectId: "testing-fb3fa",
-  storageBucket: "testing-fb3fa.appspot.com",
-  messagingSenderId: "1011352025488",
-  appId: "1:1011352025488:web:12f42df290a0553d735b15",
-  measurementId: "G-CCX4HBQ5CJ"
+  apiKey: "******************",
+  authDomain: "***************",
+  projectId: "**************",
+  storageBucket: "*****************",
+  messagingSenderId: "****************",
+  appId: "******************",
+  measurementId: "*****************"
 };
 
 // Initialize Firebase
